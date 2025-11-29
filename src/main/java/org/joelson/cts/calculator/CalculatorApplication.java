bb0f13d9b98dcfd6f0c486051de484a6dfbb0e30
Mattias Joëlson <mattias@joelson.org>
2025-11-29 12:29:54 +0100

    Rename application class.

--- src/main/java/org/joelson/cts/calculator/CalculatorApplication.java
+++ src/main/java/org/joelson/cts/calculator/CalculatorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CtsCalculatorApplication {
+public class CalculatorApplication {
 
     static void main(String[] args) {
-        SpringApplication.run(CtsCalculatorApplication.class, args);
+        SpringApplication.run(CalculatorApplication.class, args);
     }
 }
