6fa2d5d925e2dadc9eb5f5d91a8c2edde215299e
Mattias Joëlson <mattias@joelson.org>
2025-12-06 01:12:03 +0100

    Extract production print method.

--- src/main/java/org/joelson/cts/calculator/model/ImprovementCalculator.java
+++ src/main/java/org/joelson/cts/calculator/model/ImprovementCalculator.java
@@ -14,14 +14,7 @@ public class ImprovementCalculator {
         throw new InstantiationException("Should not be instantiated!");
     }
 
-    public static void calculateImprovement(Garden garden) {
-        Map<String, Float> totalProduction = new HashMap<>();
-        for (String currency : garden.getCurrencies()) {
-            totalProduction.put(currency, 0f);
-        }
-        for (Generator generator : garden.getGenerators()) {
-            generator.updateEfficiency(garden);
-        }
+    public static void printProduction(Garden garden, Map<String, Float> totalProduction) {
         for (Generator generator : garden.getGenerators().reversed()) {
             int count = generator.getCount();
             String currencyName = generator.getBaseProduction().currency();
@@ -37,6 +30,17 @@ public class ImprovementCalculator {
             System.out.printf("%s production: %.2e%n", currency, totalProduction.get(currency));
         }
         System.out.println();
+    }
+
+    public static void calculateImprovement(Garden garden) {
+        Map<String, Float> totalProduction = new HashMap<>();
+        for (String currency : garden.getCurrencies()) {
+            totalProduction.put(currency, 0f);
+        }
+        for (Generator generator : garden.getGenerators()) {
+            generator.updateEfficiency(garden);
+        }
+        printProduction(garden, totalProduction);
 
         Map<CurrencyMapping, List<Improvement>> improvements = new HashMap<>();
         for (Generator generator : garden.getGenerators().reversed()) {
@@ -52,57 +56,64 @@ public class ImprovementCalculator {
         }
 
         for (Map.Entry<CurrencyMapping, List<Improvement>> improvementsEntry : improvements.entrySet()) {
-            System.out.printf("Improvements from %s to %s:%n", improvementsEntry.getKey().from(),
-                    improvementsEntry.getKey().to());
-            List<Improvement> imp = improvementsEntry.getValue();
-            imp.sort(Comparator.comparing(Improvement::getRatio));
-            for (Improvement improvement : imp.reversed()) {
-                Amount cost = improvement.getCost();
-                float ratio = improvement.getRatio();
-                Amount increase = improvement.getIncrease();
-                float time = cost.amount() / totalProduction.get(cost.currency());
-                System.out.printf("%s: cost %s, increase %s, ratio %.7f, time %s%n",
-                        improvement.getName(), cost.asString(), increase.asString(), ratio, durationString(time));
+            calculateMappingImprovement(improvementsEntry, totalProduction, improvements);
+        }
+    }
+
+    private static void calculateMappingImprovement(
+            Map.Entry<CurrencyMapping, List<Improvement>> improvementsEntry, Map<String, Float> totalProduction,
+            Map<CurrencyMapping, List<Improvement>> improvements) {
+        System.out.printf("Improvements from %s to %s:%n", improvementsEntry.getKey().from(),
+                improvementsEntry.getKey().to());
+        List<Improvement> mappingImprovements = improvementsEntry.getValue();
+        mappingImprovements.sort(Comparator.comparing(Improvement::getRatio));
+        for (Improvement improvement : mappingImprovements.reversed()) {
+            Amount cost = improvement.getCost();
+            float ratio = improvement.getRatio();
+            Amount increase = improvement.getIncrease();
+            float time = cost.amount() / totalProduction.get(cost.currency());
+            System.out.printf("%s: cost %s, increase %s, ratio %.7f, time %s%n",
+                    improvement.getName(), cost.asString(), increase.asString(), ratio, durationString(time));
+        }
+        Improvement best = mappingImprovements.getLast();
+        System.out.printf("Best: %s, (%s)%n", best.getName(), best.getMapping());
+        List<Improvement> candidates = new ArrayList<>();
+        for (Map.Entry<CurrencyMapping, List<Improvement>> candidateEntry : improvements.entrySet()) {
+            if (!candidateEntry.getKey().to().equals(best.getCost().currency())) {
+                continue;
             }
-            Improvement best = imp.getLast();
-            System.out.printf("Best: %s, (%s)%n", best.getName(), best.getMapping());
-            List<Improvement> candidates = new ArrayList<>();
-            for (Map.Entry<CurrencyMapping, List<Improvement>> candidateEntry : improvements.entrySet()) {
-                if (!candidateEntry.getKey().to().equals(best.getCost().currency())) {
+            for (Improvement candidate : candidateEntry.getValue()) {
+                if (candidate == best) {
                     continue;
                 }
-                for (Improvement candidate : candidateEntry.getValue()) {
-                    if (candidate == best) {
-                        continue;
-                    }
-                    candidates.add(candidate);
-                    System.out.printf("  candidate %s%n", candidate.getName());
-                }
+                candidates.add(candidate);
+                System.out.printf("  candidate %s%n", candidate.getName());
             }
-            Amount bestCost = best.getCost();
-            float bestTime = bestCost.amount() / totalProduction.get(bestCost.currency());
-            float shortestTime = bestTime;
-            Improvement bestImprovement = null;
-            System.out.printf("%s: time %s%n", best.getName(), durationString(bestTime));
-            for (Improvement candidate : candidates) {
-                Amount candidateCost = candidate.getCost();
-                float candidateTime = candidateCost.amount() / totalProduction.get(candidateCost.currency());
-                Amount candidateIncrease = candidate.getIncrease();
-                float bestImprovedTime = bestCost.amount() / (totalProduction.get(candidateIncrease.currency())
-                        + candidateIncrease.amount());
-                float totalTime = candidateTime + bestImprovedTime;
-                System.out.printf("%s and %s: %s and %s = %s%n", candidate.getName(), best.getName(),
-                        durationString(candidateTime), durationString(bestImprovedTime), durationString(totalTime));
-                if (totalTime < shortestTime) {
-                    shortestTime = totalTime;
-                    bestImprovement = candidate;
-                }
-            }
-            if (bestImprovement != null) {
-                System.out.printf("Do %s before %s%n", bestImprovement.getName(), best.getName());
-            }
-
-            System.out.println();
         }
+        Amount bestCost = best.getCost();
+        float bestTime = bestCost.amount() / totalProduction.get(bestCost.currency());
+        float shortestTime = bestTime;
+        Improvement bestImprovement = null;
+        System.out.printf("%s: time %s%n", best.getName(), durationString(bestTime));
+        for (Improvement candidate : candidates) {
+            Amount candidateCost = candidate.getCost();
+            float candidateTime = candidateCost.amount() / totalProduction.get(candidateCost.currency());
+            Amount candidateIncrease = candidate.getIncrease();
+            float bestImprovedTime = bestCost.amount() / (totalProduction.get(candidateIncrease.currency())
+                    + candidateIncrease.amount());
+            float totalTime = candidateTime + bestImprovedTime;
+            System.out.printf("%s and %s: %s and %s = %s%n", candidate.getName(), best.getName(),
+                    durationString(candidateTime), durationString(bestImprovedTime), durationString(totalTime));
+            if (totalTime < shortestTime) {
+                shortestTime = totalTime;
+                bestImprovement = candidate;
+            }
+        }
+        if (bestImprovement != null) {
+            System.out.printf("Do %s before %s%n", bestImprovement.getName(), best.getName());
+
+        }
+
+        System.out.println();
     }
 }
