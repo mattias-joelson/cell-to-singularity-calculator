136614c81239d5656663d858da29c7ceb0d700b4
Mattias Joëlson <mattias@joelson.org>
2025-12-13 18:00:41 +0100

    Change member name to compoundingCost.

--- src/main/java/org/joelson/cts/calculator/model/Generator.java
+++ src/main/java/org/joelson/cts/calculator/model/Generator.java
@@ -1,9 +1,9 @@
 package org.joelson.cts.calculator.model;
 
-public record Generator(String name, Amount baseCost, float costIncrease, Amount baseProduction) {
+public record Generator(String name, Amount baseCost, float compoundingCost, Amount baseProduction) {
 
     public Amount getCost(int level) {
-        return new Amount(baseCost.currency(), (float) (baseCost.amount() * Math.pow(costIncrease, level)));
+        return new Amount(baseCost.currency(), (float) (baseCost.amount() * Math.pow(compoundingCost, level)));
     }
 
     public CurrencyMapping getMapping() {
