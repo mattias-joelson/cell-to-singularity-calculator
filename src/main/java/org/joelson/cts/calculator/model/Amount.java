a308fa3dfbdc6203ae7a7c33a14b78f957da1b66
Mattias Joëlson <mattias@joelson.org>
2026-01-06 10:07:07 +0100

    Addition on amounts, check for same currency.

--- src/main/java/org/joelson/cts/calculator/model/Amount.java
+++ src/main/java/org/joelson/cts/calculator/model/Amount.java
@@ -2,16 +2,31 @@ package org.joelson.cts.calculator.model;
 
 public record Amount(String currency, double amount) {
 
-    public Amount multiplyBy(double f) {
-        return new Amount(currency, amount * f);
+    public Amount multiplyBy(double amount) {
+        return new Amount(currency, this.amount * amount);
     }
 
-    public Amount divideBy(double f) {
-        return new Amount(currency, amount / f);
+    public Amount divideBy(double amount) {
+        return new Amount(currency, this.amount / amount);
+    }
+
+    public Amount plus(Amount that) {
+        checkCurrency(that);
+        return plus(that.amount);
+    }
+
+    public Amount plus(double amount) {
+        return new Amount(currency, this.amount + amount);
     }
 
     public String asString() {
         return (amount < 1_000_000) ?
                 String.format("%,.0f %s", amount, currency) : String.format("%.2e %s", amount, currency);
+    }
+
+    private void checkCurrency(Amount that) {
+        if (!currency.equals(that.currency)) {
+            throw new IllegalStateException("Trying to mix currencies " + currency + " and " + that.currency + ".");
+        }
     }
 }
